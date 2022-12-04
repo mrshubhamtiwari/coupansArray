b42f8c5bb3368ddf04e42569b096912cab2b551f
Shubham Tiwari <shubhamt948@gmail.com>
2022-12-04 23:25:39 +0530

    test

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -55,6 +55,21 @@ const coupans = [
 		image:
 			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/905px-Amazon_logo.svg.png?20220213013322",
 	},
+	{
+		id: 3,
+		brand: "Amazon",
+		label: "50% off on electronics",
+		coupancode: "KHHAKJS-UIYI-NHJHJNKL-MLJL",
+		addedDate:
+			date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear(),
+		expiryDate:
+			date.getDate() + 10 + "/" + date.getMonth() + "/" + date.getFullYear(),
+		addedBy: "Gautam Sharma",
+		viewCount: 120,
+		status: "Active",
+		image:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/905px-Amazon_logo.svg.png?20220213013322",
+	},
 ];
 
 function App() {
