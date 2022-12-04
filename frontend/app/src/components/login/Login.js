a61659d37c6b95f1314d3f8b417c74e832fa209e
gautam <gautamsharma.best@gmail.com>
2022-12-05 01:16:39 +0530

    Gautam Changes

--- frontend/app/src/components/login/Login.js
+++ frontend/app/src/components/login/Login.js
@@ -2,7 +2,20 @@ import React from "react";
 
 import "./login.css";
 function Login(props) {
-	return <div className="login-container">asdasdasd</div>;
+  return (
+    <div className="login-container">
+      <form>
+        <label for="Username">Username : </label>
+        <input type="text" placeholder="Enter Username" required />
+        <label for="Password">Password : </label>
+        <input type="password" placeholder="Enter Password" required />
+        <button type="submit" value="Submit" className="btn btn-danger">
+          {" "}
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
